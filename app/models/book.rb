df965b6d901a271eec69d23715c97ecc6c8c07bc
Ramiro Lopes Canadell <ramirolopescanadell@gmail.com>
2021-02-11 13:13:30 -0300

    Nombre de notas y libros mas largos

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
   
   validates :name, null:false 
   validates_uniqueness_of :name, scope: :user_id
-  validates_length_of :name, maximum: 30, message: "the name of the book must have a maximum of 30 characters"
+  validates_length_of :name, maximum: 50, message: "the name of the book must have a maximum of 30 characters"
 
   scope :books_per_user, -> (user_id){ where("user_id == ?",user_id )} 
   
--- app/models/note.rb
+++ app/models/note.rb
@@ -3,7 +3,7 @@ class Note < ApplicationRecord
 
 	validates :name, null:false 
   	validates_uniqueness_of :name, scope: :book_id
-	validates_length_of :name, maximum: 30, message: "- The name of the note must have a maximum of 30 characters"
+	validates_length_of :name, maximum: 50, message: "- The name of the note must have a maximum of 30 characters"
 
 	scope :notes_per_book, -> (book_id){where("book_id == ?", book_id)}
 	scope :notes_per_user, -> (user_id) {joins(:book).where("user_id == ?", user_id).select("notes.*, books.name as book_name")}
